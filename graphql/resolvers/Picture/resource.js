59184ec48bc2529333c7c3765b43b59f8bd47f77
Abir Taheer <abir@taheer.me>
2020-12-29 09:56:55 -0500

    removed unnecessary import from resource resolver

--- graphql/resolvers/Picture/resource.js
+++ graphql/resolvers/Picture/resource.js
@@ -1,4 +1,3 @@
 import CloudinaryResource from "../../../models/cloudinaryResource";
-import cloudinaryResourceLoader from "../../../utils/dataloaders/cloudinaryResourceLoader";
 
 export default picture => CloudinaryResource.idLoader.load(picture.resourceId);
