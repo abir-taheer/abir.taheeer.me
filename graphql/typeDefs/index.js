617218c2e2b693d3e776a37bd2274672fb1d606b
Abir Taheer <abir@taheer.me>
2020-12-29 02:50:23 -0500

    fixed import for person

--- graphql/typeDefs/index.js
+++ graphql/typeDefs/index.js
@@ -2,7 +2,7 @@ import Query from "./Query";
 import scalars from "./scalars";
 import Album from "./Album";
 import CloudinaryResource from "./CloudinaryResource";
-import Person from "../../models/person";
+import Person from "./Person";
 import Picture from "./Picture";
 
 const typeDefs = [Album, CloudinaryResource, Person, Picture, Query, scalars];
